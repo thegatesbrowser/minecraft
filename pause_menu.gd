8c94b7f43b4e04e38bae8fa805aea6f87a2791bf
thetwocs13115 <snapgamesstudio@gmail.com>
2025-02-14 19:11:18 +1100

    added control settings

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,4 +1,4 @@
-extends PanelContainer
+extends Control
 
 var played_ani:bool = false
 
@@ -8,14 +8,22 @@ func _process(delta: float) -> void:
 		visible = !visible
 		
 		if visible:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			if !played_ani:
 				GlobalAnimation._tween(self,"bounce_in",.2)
 				played_ani = true
 			show()
 		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			$Settings.hide()
 			played_ani = false
 			hide()
 		
 func _on_main_menu_pressed() -> void:
 	await get_tree().process_frame
+	Globals.paused = false
 	get_tree().change_scene_to_file("res://menus/Menu.tscn")
+
+
+func _on_settings_pressed() -> void:
+	$Settings.visible = !$Settings.visible
