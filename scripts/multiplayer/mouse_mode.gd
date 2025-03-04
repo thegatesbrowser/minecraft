6210afeb39fa096eda573e9c30f06e1f32028a47
Nordup <nordup.ondr@gmail.com>
2025-03-04 11:26:26 +0400

    change menu and debug inputs

--- scripts/multiplayer/mouse_mode.gd
+++ scripts/multiplayer/mouse_mode.gd
@@ -8,8 +8,9 @@ func _ready() -> void:
 
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("show_mouse"): set_captured(false)
+	# if Input.is_action_just_pressed("show_mouse"): set_captured(false)
 	#if Input.is_action_just_released("show_mouse"): set_captured(true)
+	pass
 
 
 func on_visibility_changed() -> void:
