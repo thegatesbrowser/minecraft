7d6fdcad9950eaae05dcba15ffdfcfc7ed3e0194
Nordup <nordup.ondr@gmail.com>
2025-03-09 14:01:01 +0400

    fix player spawner

--- scripts/ui/stats_ui_update.gd
+++ scripts/ui/stats_ui_update.gd
@@ -2,15 +2,16 @@ extends Control
 
 @onready var healthbar: TextureProgressBar = $healthbar
 @onready var hungerbar: TextureProgressBar = $hungerbar
+@onready var player_spawner: PlayerSpawner = get_tree().get_first_node_in_group("PlayerSpawner")
 
+var your_player: Player
 
-@export var player_spawner:PlayerSpawner
-var your_player:Player
 
 func find_player():
 	for id in player_spawner.players:
 		if id == multiplayer.get_unique_id():
 			your_player = player_spawner.players[id].player
+
 
 func _process(delta: float) -> void:
 	if your_player == null: return
