6a7dc61b7078286648438bb8f3a54ebaba4949ca
Nordup <nordup.ondr@gmail.com>
2025-03-10 21:42:54 +0400

    code style

--- scripts/other/portal_manager.gd
+++ scripts/other/portal_manager.gd
@@ -2,35 +2,35 @@ extends Node
 
 var server_info := {}
 
+@export var UI: Control
 
-@export var UI:Control
 
 func _ready() -> void:
 	Globals.create_portal.connect(add_new_portal)
 	Globals.add_portal_url.connect(add_portal_url)
 	Globals.enter_portal.connect(check_portal)
 
-func add_new_portal(id:Vector3):
-	
-	send_to_server.rpc_id(1,id)
-	
 
-	#portal_url.id = id
-	#UI.add_child(portal_url)
-	#
+func add_new_portal(id:Vector3):
+	send_to_server.rpc_id(1,id)
+
+
 func check_portal(id:Vector3):
 	server_check_portal.rpc_id(1,id)
-	
+
+
 @rpc("any_peer","call_local")
 func send_to_server(id:Vector3,url := "https://thegates.io/worlds/world.gate"):
 	server_info[id] = {
 		"url": url
 	}
 	print(server_info[id])
-	
+
+
 func add_portal_url(id:Vector3,url:String):
 	send_to_server.rpc_id(1,id,url)
-	
+
+
 @rpc("any_peer","call_local")
 func server_check_portal(id:Vector3):
 	if server_info.has(id):
@@ -44,4 +44,3 @@ func give_client(info:Dictionary):
 	if get_tree().has_method("send_command"):
 		get_tree().send_command("open_gate", [info.url])
 	pass
-	
--- scripts/ui/portal_url.gd
+++ scripts/ui/portal_url.gd
@@ -2,14 +2,17 @@ extends Control
 
 var id:Vector3
 
+
 func _ready() -> void:
 	Globals.open_portal_url.connect(open)
-	
+
+
 func _on_url_text_submitted(new_text: String) -> void:
 	Globals.add_portal_url.emit(id,new_text)
 	$PanelContainer/MarginContainer/VBoxContainer/url.text = ""
 	hide()
 	Globals.paused = false
+
 
 func open(_id):
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
